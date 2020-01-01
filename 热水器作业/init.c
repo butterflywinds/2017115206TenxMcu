3d2a1b23581a6a5db2049825dfcccc44912de7b7
butterflywinds <825601861@qq.com>
2020-01-01 22:46:47 +0800

    初始化源文件

---
+++ 热水器作业/init.c
@@ -0,0 +1,45 @@
+#define __init_c
+#include "includeAll.h"
+//=============================================================================
+void SysInit() {
+	F_clearWDT();
+  
+  CLKCON = 0x03; // Clock div 1  7.3728Mhz
+  STPPCK = 0;
+  STPFCK = 0;
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  SELFCK = 1;
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  // IO 口初始化
+	P1MODL = 0xaa;		//将模式设置为推挽输出
+	P1MODH = 0xaa;
+	//P1MODL = 0xa8;		//上拉输入
+
+  //  7.3728Mhz/2 = 3686400Hz
+  //  3686400Hz/3686.4 = 1000Hz = 1ms timer2 
+  TH2 = (65536 - 922) / 256;
+  RCP2H = TH2;
+  TL2 = (65536 - 922) % 256;
+  RCP2L = TL2;
+  TR2 = 1;
+  ET2 = 1;
+  EA = 1; 
+}
+//=============================================================================
+void VarsInit() {}
+
+
+
