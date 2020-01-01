b0cfcc70a662f787ee3cceaedb5a63b84669faea
butterflywinds <825601861@qq.com>
2020-01-01 22:50:17 +0800

    主程序头文件

---
+++ 热水器作业/main.h
@@ -0,0 +1,24 @@
+#ifndef __main_h
+#define __main_h
+// Hal: exp: #define P_led P10 -----------------
+
+// Const: exp: #define D_data 1 ----------------
+
+// Globle Var -----------------------------------------
+#ifdef __main_c
+#else
+
+
+#endif
+
+
+// Action Macro: exp: #define F_getData() ------
+
+// Function ------------------------------------
+void delayMs(uint16_t time);
+void TimeProcess();
+void TaskSetting();
+void TaskProcess();
+void DisplayProcess();
+
+#endif
