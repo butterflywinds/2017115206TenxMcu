721644da7092372121956de05a7b4da6071e2ce5
butterflywinds <825601861@qq.com>
2020-01-02 15:44:15 +0800

    增加按键2和按键3的扫描

--- 热水器作业/keys.c
+++ 热水器作业/keys.c
@@ -4,10 +4,13 @@
 //按键信息获取
 void GetKeys() {
 	static uint8_t tempKeyValue1 = D_keyNull;
+        static uint8_t tempKeyValue2 = D_keyNull;
+	static uint8_t tempKeyValue3 = D_keyNull;
 
         delayMs(20);
         P3MODH &= P34MOD_Mask;
-
+        P3MODL &= P32MOD_Mask;
+        P1MODH &= P17MOD_Mask;
 	_nop_();
 	_nop_();
 	_nop_();
@@ -19,6 +22,17 @@ void GetKeys() {
 		keyValue1 = tempKeyValue1;
 		tempKeyValue1 = D_keyNull;
 	}
-	
+	if(P_key2 == 0){
+		tempKeyValue2 = D_keyValue1;
+	}	else{
+		keyValue2 = tempKeyValue2;
+		tempKeyValue2 = D_keyNull;
+	}
+	if(P_key3 == 0){
+		tempKeyValue3 = D_keyValue1;
+	}	else{
+		keyValue3 = tempKeyValue3;
+		tempKeyValue3 = D_keyNull;
+	}
 	P3MODH |= P34MOD_2;
 }
